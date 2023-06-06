ca62a41594781fec2d3dcc3d13a7e16f906b726c
Marimar03 <md.acuna@alumno.um.edu.ar>
2023-06-06 11:56:25 -0300

    subo tarjeta sube

---
+++ tarjeta_sube.py
@@ -0,0 +1,52 @@
+PRECIO_TICKET = 70
+PRIMARIO = "primario"
+SECUNDARIO = "secundario"
+JUBILADO = "jubilado"
+ACTIVADO = "activado"
+DESACTIVADO = "desactivado"
+UNIVERSITARIO = "universitario"
+
+DESCUENTOS = {PRIMARIO: 50, SECUNDARIO: 40, UNIVERSITARIO: 30, JUBILADO: 25,}
+
+class NoHaySaldoException (Exception):
+    pass 
+
+class UsuarioDesactivadoException (Exception):
+    pass
+
+class EstadoNoExistenteException (Exception):
+    pass
+
+class Sube:
+    def __init__(self):
+        self.saldo = 1000
+        self.grupo_beneficiario = None
+        self.estado = ACTIVADO
+        
+    def obtener_precio_ticket(self): #obtener el precio con descuento y sin descuento
+        if self.grupo_beneficiario == None:
+            return PRECIO_TICKET
+        elif self.grupo_beneficiario == PRIMARIO:
+            return PRECIO_TICKET * 0.5
+        elif self.grupo_beneficiario == SECUNDARIO:
+            return PRECIO_TICKET * 0.6
+        elif self.grupo_beneficiario == UNIVERSITARIO:
+            return PRECIO_TICKET * 0.7
+        elif self.grupo_beneficiario == JUBILADO:
+            return PRECIO_TICKET * 0.75
+
+    def pagar_pasaje(self):
+        if self.estado == DESACTIVADO:
+            raise UsuarioDesactivadoException()
+        elif self.saldo < self.obtener_precio_ticket() :
+            raise NoHaySaldoException()
+        elif self.grupo_beneficiario == PRIMARIO or SECUNDARIO or UNIVERSITARIO or JUBILADO and self.estado == ACTIVADO:
+            self.saldo -= self.obtener_precio_ticket()
+            return self.saldo
+    
+    def cambiar_estado(self,estado):
+        validos = [ACTIVADO, DESACTIVADO]
+        if not estado in validos:
+            raise EstadoNoExistenteException()
+        self.estado = estado 
+
